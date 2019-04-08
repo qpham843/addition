8cd3fae3001fdc7cd665ee3b26155db7b5a311da
qpham843 <32086045+qpham843@users.noreply.github.com>
2019-04-08 15:06:52 -0700

    Added the first DnD Character test and class.

---
+++ src/DnDCharacter.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+public class DnDCharacter {
+    public static String[] names = new String[]{"Bob"};
+    public static String[] dndClasses = new String[]{"Barbarian", "Bard", "Cleric", "Druid", "Fighter","Monk", "Paladin", "Ranger", "Rogue", "Sorcerer", "Warlock", "Wizard"};
+
+    String name;
+    String dndClass;
+    Map<String, Integer> attributes;
+
+    DnDCharacter(String name, String dndClass, Map<String, Integer> attributes) {
+        this.name = name;
+        this.dndClass = dndClass;
+        this.attributes = attributes;
+    }
+
+    public static DnDCharacter randomChar() {
+        String randName = "";
+        String randClass = "";
+        Map<String, Integer> randAttributes = new HashMap<String, Integer>();
+        randAttributes.put("Strength", new Random().nextInt(20));
+        randAttributes.put("Dexterity", new Random().nextInt(20));
+        randAttributes.put("Constitution", new Random().nextInt(20));
+        randAttributes.put("Intelligence", new Random().nextInt(20));
+        randAttributes.put("Wisdom", new Random().nextInt(20));
+        randAttributes.put("Charisma", new Random().nextInt(20));
+        return new DnDCharacter(randName, randClass, randAttributes);
+    }
+
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof DnDCharacter)) {
+            return false;
+        }
+
+        DnDCharacter otherChar = (DnDCharacter) other;
+        if (this.name.equals(otherChar.name) && this.dndClass.equals(otherChar.dndClass) && this.attributes.equals(otherChar.attributes)) {
+            return true;
+        }
+        return false;
+    }
+}
---
+++ test/DnDCharacterTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class DnDCharacterTest {
+    @Test
+    public void DnDCharacterIsEqualToItself() {
+        Map<String,Integer> attributes = new HashMap<String, Integer>();
+        attributes.put("Strength", 0);
+        attributes.put("Dexterity", 0);
+        attributes.put("Constitution", 0);
+        attributes.put("Intelligence", 0);
+        attributes.put("Wisdom", 0);
+        attributes.put("Charisma", 0);
+        DnDCharacter myChar = new DnDCharacter("Name", "Class", attributes);
+        assertEquals(myChar, myChar);
+    }
+}
