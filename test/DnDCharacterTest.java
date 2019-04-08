f84524aa807d8655b74a9a94642441f8107b820d
qpham843 <32086045+qpham843@users.noreply.github.com>
2019-04-08 16:00:12 -0700

    Added another random character test.

--- test/DnDCharacterTest.java
+++ test/DnDCharacterTest.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class DnDCharacterTest {
     @Test
@@ -24,5 +25,19 @@ public class DnDCharacterTest {
         DnDCharacter randChar = DnDCharacter.randomChar();
         DnDCharacter myChar = new DnDCharacter(randChar.name, randChar.dndClass, randChar.attributes);
         assertEquals(randChar, myChar);
+    }
+
+    @Test
+    public void randomDnDCharacterIsNotEqualToAPremadeCharacterWithNonStandardNameAndClass() {
+        Map<String,Integer> attributes = new HashMap<String, Integer>();
+        attributes.put("Strength", 0);
+        attributes.put("Dexterity", 0);
+        attributes.put("Constitution", 0);
+        attributes.put("Intelligence", 0);
+        attributes.put("Wisdom", 0);
+        attributes.put("Charisma", 0);
+        DnDCharacter myChar = new DnDCharacter("Name", "Class", attributes);
+        DnDCharacter randChar = DnDCharacter.randomChar();
+        assertNotEquals(randChar, myChar);
     }
 }
