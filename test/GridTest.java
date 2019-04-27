9a91ae5081b77beab9188a9cd484c8990ac69106
qpham843 <32086045+qpham843@users.noreply.github.com>
2019-04-26 17:19:46 -0700

    Added an inequality test for the grids.

--- test/GridTest.java
+++ test/GridTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class GridTest {
     @Test
@@ -15,6 +16,13 @@ public class GridTest {
         Grid grid1 = new Grid(1, 1);
         Grid grid2 = new Grid(1, 1);
         assertEquals(grid1, grid2);
+    }
+
+    @Test
+    public void twoGridsOfUnequalSizeAreNotEqual() {
+        Grid grid1 = new Grid(1, 1);
+        Grid grid2 = new Grid(2, 2);
+        assertNotEquals(grid1, grid2);
     }
 
     @Test
